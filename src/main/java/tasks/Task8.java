35edc0244789db9b7c085379453a2d0a3c49f8bb
kochachii <nbarinov956@gmail.com>
2024-12-13 18:22:36 +0300

    small fixes
    Task9
    
    Task8
    
    Task7
    
    Task6
    
    Task5
    
    Task4 + small fixes
    
    Task3
    
    Task2
    
    Task1

--- src/main/java/tasks/Task8.java
+++ src/main/java/tasks/Task8.java
@@ -4,8 +4,13 @@ import common.Person;
 import common.PersonService;
 import common.PersonWithResumes;
 import common.Resume;
+
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
   Еще один вариант задачи обогащения
@@ -21,7 +26,19 @@ public class Task8 {
   }
 
   public Set<PersonWithResumes> enrichPersonsWithResumes(Collection<Person> persons) {
-    Set<Resume> resumes = personService.findResumes(Set.of());
-    return Set.of();
+    Set<Integer> personIds = persons.stream()
+        .map(Person::id)
+        .collect(Collectors.toSet());
+    Map<Integer, Set<Resume>> resumesByPersonId = personService.findResumes(personIds).stream()
+        .collect(Collectors.groupingBy(
+            Resume::personId,
+            Collectors.toSet()
+        ));
+    return persons.stream()
+        .map(person -> {
+          Set<Resume> personResumes = resumesByPersonId.computeIfAbsent(person.id(), k -> Collections.emptySet());
+          return new PersonWithResumes(person, personResumes);
+        })
+        .collect(Collectors.toSet());
   }
 }
